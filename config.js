d0c60b7b413eaa2bf2bdc4790f2afc946572c21e
Anthony Nguyen <ahnguyen@gmail.com>
2021-04-11 21:36:18 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ function watermark(svg) {
         .attr("transform", "translate("+(pad.l+W/2)+","+(pad.t+H/2-20)+")")
         .attr("opacity",0.2);
   wm.append("image")
-      .attrs({x:-125, y:0, width:250, height:250, "xlink:href":"http://headphones.audiodiscourse.com/ad.png"});
-   //wm.append("text")
-       //.attrs({x:0, y:100, "font-size":40, "text-anchor":"middle"})
-        //.text("Audio Discourse");
+      .attrs({x:-125, y:0, width:250, height:250, "xlink:href":"ad.png"});
+   wm.append("text")
+       .attrs({x:250, y:170, "font-size":16, "font-family":"tahoma", "text-anchor":"middle"})
+        .text("http://www.AudioDiscourse.com");
 }
 
 const max_channel_imbalance = 5;
