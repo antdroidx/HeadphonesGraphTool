a0cb0cfbea33cd9588a8186b7e6217ad631aaa5f
Anthony Nguyen <ahnguyen@gmail.com>
2021-04-14 08:43:55 -0700

    config test

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const DIR = "data/";
+const DIR = "https://headphones.audiodiscourse.com/data/";
 // Get data (as a list of [x,y] pairs) from an FR file
 const tsvParse = fr => d3.tsvParseRows(fr).slice(15,482)
                        .map(r=>r.map(d=>+d));
